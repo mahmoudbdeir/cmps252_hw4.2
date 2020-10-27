0b8b6d114b1fa117f437dccc33000ff04d1b109d
Hadi Hachem <hjh09@mail.aub.edu>
2020-10-27 20:17:03 +0200

    Added HW4.2

--- src/cmps252/HW4_2/FileParser.java
+++ src/cmps252/HW4_2/FileParser.java
@@ -1,5 +1,34 @@
 package cmps252.HW4_2;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class FileParser {
-	//TODO add your code here
+	
+	public static List<Customer> getCustomers(String file) throws FileNotFoundException{
+		List<Customer> Customers = new LinkedList<Customer>();
+		
+		try (Scanner scanner = new Scanner(new File(file));){
+			int lineCount = 0;
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				if(lineCount++==0) continue;
+				String[] fields = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+				if(fields==null) {
+					continue;
+				}
+				Customer sample = new Customer(clean(fields[0]), clean(fields[1]), clean(fields[2]), clean(fields[3]), clean(fields[4]), clean(fields[5]), clean(fields[6]), clean(fields[7]), clean(fields[8]), clean(fields[9]), clean(fields[10]), clean(fields[11]));
+				Customers.add(sample);
+			}
+		}
+		return Customers;
+	}
+
+	private static String clean(String string) {
+		return string.substring(1, string.length()-1);
+	}
+
 }
