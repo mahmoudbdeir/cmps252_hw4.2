bfe849c3f46f59d34e33a174d11fd4a88da45a39
OmarJazzar <68284766+OmarJazzar@users.noreply.github.com>
2020-10-27 18:52:25 +0200

    Update FileParser.java

--- src/cmps252/HW4_2/FileParser.java
+++ src/cmps252/HW4_2/FileParser.java
@@ -1,5 +1,42 @@
 package cmps252.HW4_2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileParser {
-	//TODO add your code here
+	private String name;
+
+	public FileParser(String file_name) {
+		name = file_name;
+	}
+
+	public static List<Customer> getCustomers(String file) throws FileNotFoundException {
+		List<Customer> Customers = new ArrayList<Customer>();
+
+		try (Scanner scan = new Scanner(new File(file));) {
+			int lineCount = 0;
+			while (scan.hasNextLine()) {
+				String line = scan.nextLine();
+				if (lineCount++ == 0)
+					continue;
+
+				String[] fields = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+				if (fields == null) {
+					continue;
+				}
+				Customer s = new Customer(clean(fields[0]), clean(fields[1]), clean(fields[02]), clean(fields[3]),
+						clean(fields[4]), clean(fields[5]), clean(fields[6]), clean(fields[7]), clean(fields[8]),
+						clean(fields[9]), clean(fields[10]), clean(fields[11]));
+				Customers.add(s);
+			}
+		}
+		return Customers;
+	}
+
+	private static String clean(String s) {
+		return s.substring(1, s.length() - 1);
+	}
 }
