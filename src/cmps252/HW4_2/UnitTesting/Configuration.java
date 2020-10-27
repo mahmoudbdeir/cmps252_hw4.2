45b002264f9f5296c167d996fdee68b0e3612e0a
Patrick Wehbe <pew02@mail.aub.edu>
2020-10-27 16:32:32 +0200

    init

--- src/cmps252/HW4_2/UnitTesting/Configuration.java
+++ src/cmps252/HW4_2/UnitTesting/Configuration.java
@@ -2,5 +2,5 @@ package cmps252.HW4_2.UnitTesting;
 
 public class Configuration {
 	//TODO change the file path to the file located on your disk
-	public static final String CSV_File = "C:\\temp\\4\\cmps252_hw4.2\\5000.csv";
+	public static final String CSV_File = "C:\\Users\\User\\Desktop\\AUB\\Junior\\Fall 2020-2021\\CMPS 252\\Assignments\\asst4.2\\Homework 4.2\\src\\5000.csv";
 }
