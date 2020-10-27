0d91625bd6a4e5b2de5f0b1b1225dcc1b53731e3
OmarJazzar <68284766+OmarJazzar@users.noreply.github.com>
2020-10-27 18:51:03 +0200

    Update Customer.java

--- src/cmps252/HW4_2/Customer.java
+++ src/cmps252/HW4_2/Customer.java
@@ -1,6 +1,81 @@
 package cmps252.HW4_2;
 
 public class Customer {
-	//TODO add your code here	
-	
+	private String FirstName;
+	private String LastName;
+	private String Company;
+	private String Address;
+	private String City;
+	private String Country;
+	private String State;
+	private String ZIP;
+	private String Phone;
+	private String Fax;
+	private String Email;
+	private String Web;
+
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public String getCompany() {
+		return Company;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public String getCity() {
+		return City;
+	}
+
+	public String getCountry() {
+		return Country;
+	}
+
+	public String getState() {
+		return State;
+	}
+
+	public String getZIP() {
+		return ZIP;
+	}
+
+	public String getPhone() {
+		return Phone;
+	}
+
+	public String getFax() {
+		return Fax;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public String getWeb() {
+		return Web;
+	}
+
+	public Customer(String firstName, String lastName, String company, String address, String city, String country,
+			String state, String zIP, String phone, String fax, String email, String web) {
+		FirstName = firstName;
+		LastName = lastName;
+		Company = company;
+		Address = address;
+		City = city;
+		Country = country;
+		State = state;
+		ZIP = zIP;
+		Phone = phone;
+		Fax = fax;
+		Email = email;
+		Web = web;
+	}
+
 }
